dc08c6a959397a1825fa9960d1dcf498feb203d5
José Maia da Silva Neto <maianeto2014@gmail.com>
2022-09-26 22:14:33 -0400

    update files

---
+++ lop/prova5.1_itp.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+
+    int arr[50], num, x, y, temp;
+
+
+    scanf("%d", &num);
+
+
+    for (x = 0; x < num; x++)
+        scanf("%d", &arr[x]);
+
+    for (x = 0; x < num; x++)
+        printf("%d ", arr[x]);
+    
+    printf("\n");
+
+    for (x = 0; x < num - 1; x++){
+
+        for (y = 0; y < num - x - 1; y++){
+
+            if (arr[y] > arr[y + 1]){
+
+                temp = arr[y];
+
+                arr[y] = arr[y + 1];
+
+                arr[y + 1] = temp;
+            }
+        }
+        for (int j = 0; j < num; j++){
+
+            printf("%d ", arr[j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
---
+++ lop/prova5.2_itp.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+int main(){
+    int qte_alunos;
+
+    scanf("%d", &qte_alunos);
+
+    int chamada[qte_alunos];
+    double media[qte_alunos];
+
+    int aprovados[qte_alunos];
+    int rec[qte_alunos];
+    int reprovados[qte_alunos];
+
+    int control_var_aprov = 0;
+    int control_var_rec = 0;
+    int control_var_reprov = 0;
+
+
+    // Preenchimento do vetor
+    for (int i = 0; i < qte_alunos; i++){
+        scanf("%d - %lf", &chamada[i], &media[i]);
+    }
+
+    // Verifica situação do aluno
+    for (int i = 0; i < qte_alunos; i++){
+        if(media[i] >= 7.0){
+            aprovados[control_var_aprov] = i;
+            control_var_aprov++;
+        }
+        else if((media[i] >= 5.0) && (media[i] < 7.0)){
+            rec[control_var_rec] = i;
+            control_var_rec++;
+        }
+        else{
+            reprovados[control_var_reprov] = i;
+            control_var_reprov++;
+        }
+    }
+
+
+    // Imprime os aprovados
+    for (int i = 0; i < control_var_aprov; i++){
+        if(i == (control_var_aprov - 1)){
+            printf("Aprovados: %d (%.1lf) ", chamada[aprovados[i]], media[aprovados[i]]);
+        }
+        else {
+            printf("Aprovados: %d (%.1lf), ", chamada[aprovados[i]], media[aprovados[i]]);
+
+        }
+    }
+
+    printf("\n");
+
+    // Imprime os alunos em rec
+    for (int i = 0; i < control_var_rec; i++){
+        if(i == (control_var_rec - 1)){
+            printf("Recuperacao: %d (%.1lf) ", chamada[rec[i]], media[rec[i]]);
+        }
+        else {
+            printf("Recuperacao: %d (%.1lf), ", chamada[rec[i]], media[rec[i]]);
+        }
+    }
+    printf("\n");
+
+    // Imprime os reprovados
+    for (int i = 0; i < control_var_reprov; i++){
+        if(i == (control_var_reprov - 1)){
+            printf("Reprovados: %d (%.1lf) ", chamada[reprovados[i]], media[reprovados[i]]);
+        }
+        else {
+            printf("Reprovados: %d (%.1lf), ", chamada[reprovados[i]], media[reprovados[i]]);
+        }
+    }
+    
+    printf("\n");
+
+    return 0;
+}
