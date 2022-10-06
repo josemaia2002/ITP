bc88b96975c3169154e40880af6014176e510cba
José Maia da Silva Neto <maianeto2014@gmail.com>
2022-10-05 21:53:08 -0300

    update files

---
+++ lop/listas/lista3.3_itp.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int velocidade_relativa(int velocidade1, int velocidade2){
+	int v_rel; 
+	v_rel = velocidade1 - velocidade2;
+
+	return v_rel;
+}
+
+int calc_tempo(int distancia, int velocidade){
+	int tempo;
+	tempo = distancia / velocidade;
+
+	return tempo;
+}
+
+int main(){
+	int x, v1, v2;
+
+	scanf("%d %d %d", &x, &v1 , &v2);
+
+	if(v1 > v2){
+		printf("%ds\n", calc_tempo(x, velocidade_relativa(v1, v2)));
+
+	}
+	else{
+		printf("Impossível\n");
+	}
+		
+	return 0;
+}
